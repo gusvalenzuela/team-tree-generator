6799af01df5c9a6bc2260fd4ce95b662cfd7876e
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-03-12 18:28:20 -0700

    adding screenshot

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TEAM TREE GENERATOR
 > A Project by Gus Valenzuela
 
-[Main Screenshot(JPG)](Link)
+[Main Screenshot(./screenshot01.png)]()
 
 ## Description
 This is Team-Tree Generator. A simple survey of a team's member's basic information like name, role, and email that is used to render a web page (HTML). This web page may be used to reference like virtual business cards or to quickly update a team's webpage.
@@ -10,5 +10,7 @@ A Node CLI which asks user for GitHub username and then gathers the user's GitHu
 
 
 ## Installation
+
+> run with "node app"
 
 ## 
--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const render = require("./lib/htmlRenderer");
 
 const separator = `=`.repeat(42)
 const alphaNumArray = `0123456789`.split("")
+// let count = 0
+
 // maybe for later
 // const generateAlphaNumID = () => {
 //     const result = []
@@ -22,6 +24,8 @@ const alphaNumArray = `0123456789`.split("")
 //     }
 //     return result.join(``)
 // }
+const welcomeMessage = `\nWelcome to Team-Tree Generator.\n${separator}\nA simple survey of team member's basic info \nlike name, role, and email which is used to render a web page (HTML).\n${separator}\n`
+// const goodbyeMessage = `\nThank you for using Team-Tree Generator.\n\nThe information for ${allMembers} will be used to fill a simple web page.\nCheck for an "output" folder.\n\n\\^_^/`
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -29,26 +33,27 @@ const questions = [
     {
         name: `intro`,
         // type: `confirm`,
-        message: `\nWelcome to Team-Tree Generator.\n${separator}\nA simple survey of a team's member's basic information \nlike name, role, and email that is used to render a web page (HTML).\n${separator}\n`,
-        // [n = Advanced Mode: enter all data in one line (feature coming soon)]
-        // when: count < 1,
+        message: welcomeMessage,
     },
-    // {
-    //     name: `advMode`,
-    //     type: `input`,
-    //     message: `\nI'm sorry I just can't do that. \nAdvanced features are currently under production. Come back soon\n==================================================================`,
-    //     // validate: function (){},
-    //     when: answers => !answers.intro,
-    // },
     {
         name: `projectname`,
         type: `input`,
         message: `What is the name the project this team will be working on?`,
         default: `Untitled Project`
     },
-
+    {
+        name: `selectMode`,
+        type: `list`,
+        message: `Select which mode to use:`,
+        choices: [`Enter team member data one by one (recommended)`, new inquirer.Separator(`-------`), `Enter data in one line [comma separated values]`],
+        filter: async input => {
+            if (input === `Enter data in one line [comma separated values]`) {
+                return false
+            }
+            return true
+        }
+    }
 ]
-let count = 0
 const questionsTeam = [
     {
         name: `name`,
@@ -65,7 +70,7 @@ const questionsTeam = [
         name: `role`,
         type: `list`,
         message: `Is this team member a Manager, Engineer, or Intern?`,
-        choices: [`Manager`, `Engineer`, `Intern`, new inquirer.Separator(`-------`), `Other (Employee)`],
+        choices: [`Manager`, `Engineer`, `Intern`, new inquirer.Separator(`-------`), `(Other Employee) < saved but not rendered in web page >`],
         // default: `Other (Employee)`
     },
     {
@@ -111,72 +116,195 @@ const questionsTeam = [
     },
     {
         name: `askAgain`,
-        type: `confirm`,
+        type: `list`,
         message: `Would you like to enter another team member's information?`,
-        // default: generateID,
-        // validate: async input => {
-        //     if (!input || input === NaN || isNaN(input)) {
-        //         return `An ID is required (Must be a whole number)`
-        //     }
-        //     return true
-        // },
+        choices: [`Yes, add another team member.`, new inquirer.Separator(`-------`), `No, render my web page.`],
+        filter: async input => {
+            if (input === `No, render my web page.`) {
+                return false
+            }
+            return true
+        }
     }
 ]
 const employeesArray = []
+const employeesArrayQuick = []
 const askUser = async () => {
     try {
-        const { projectname } = await inquirer.prompt(questions)
+        const { projectname, selectMode } = await inquirer.prompt(questions)
 
         let keepAsking = true
+        console.log(separator)
+
         while (keepAsking) {
-            const { name, id, email, role, school, github, officenum, askAgain } = await inquirer.prompt(questionsTeam)
-            count++
+            if (selectMode) {
+                const { name, id, email, role, school, github, officenum, askAgain } = await inquirer.prompt(questionsTeam)
+                count++
+                console.log(separator)
+
+                // make new employee obj depending on role of employee (using answers)
+                switch (role) {
+                    case `Manager`:
+                        employeesArray.push(new Manager(name, id, email, officenum))
+                        break
+                    case `Engineer`:
+                        employeesArray.push(new Engineer(name, id, email, github))
+                        break
+                    case `Intern`:
+                        employeesArray.push(new Intern(name, id, email, school))
+                        break
+                    default:
+                        employeesArray.push(new Employee(name, id, email))
+                        break
+                }
+                if (!askAgain) {
+                    keepAsking = false
+                    renderMemberList(employeesArray)
+                }
+                employeesArray.unshift(projectname)
+                const renderedhtml = render(employeesArray)
+
+                fs.writeFile(outputPath, renderedhtml, err => {
+                    if (err) throw err
+                })
+            } else {
+                const { compactAnswer, addMore } = await inquirer.prompt(quickQuestions)
+                const eArray = compactAnswer.split(`,`).map(data => data.trim())
+                // console.log(eArray)
+                const role = eArray[3]
+                const name = eArray[0]
+                const id = eArray[1]
+                const email = eArray[2]
+                const wildcard = eArray[eArray.length - 1]
+
+                console.log(separator)
+                // make new employee obj depending on role of employee (using answers)
+                switch (role.toLowerCase()) {
+                    case `manager`:
+                        employeesArrayQuick.push(new Manager(name, id, email, wildcard))
+                        break
+                    case `engineer`:
+                        employeesArrayQuick.push(new Engineer(name, id, email, wildcard))
+                        break
+                    case `intern`:
+                        employeesArrayQuick.push(new Intern(name, id, email, wildcard))
+                        break
+                    default:
+                        employeesArrayQuick.push(new Employee(name, id, email))
+                        break
+                }
+
+                if (!addMore) {
+                    keepAsking = false
+                    renderMemberList(employeesArrayQuick)
+                }
+                employeesArrayQuick.unshift(projectname)
+                const renderedhtml = render(employeesArrayQuick)
+
+                fs.writeFile(outputPath, renderedhtml, err => {
+                    if (err) throw err
+                })
+            }
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+const renderMemberList = (array) => {
+    let lastElement = array.length - 1
+    let lastPerson = array[lastElement].name.trim().split(` `)[0]
+
+    // adding a suffix to last element to "tag it" in case there are two team members with same name
+    array[lastElement].name = `${lastPerson}++ ${array[lastElement].name.trim().split(` `)[1]}`
+
+    let allMembers = array.map(data => data.name.split(` `)[0]).join(`, `)
+
+    // if there is more than one team member, then find the last person and adhere an ampersond adding emphasis and alliteration
+    if (array.length > 2) {
+        allMembers = allMembers.replace(`${lastPerson}++`, `& ${lastPerson}`)
+    } else if (array.length > 1) {
+        allMembers = allMembers.replace(`, ${lastPerson}++`, ` & ${lastPerson}`)
+    }
+    console.log(`\nThank you for using Team-Tree Generator.\n\nThe information for ${allMembers} will be used to fill a simple web page.\nLook for an "output" folder with a "team.html" file.\n\n\\^_^/`)
+
+    // put the first name of last person back as it was, sans '++'
+    array[lastElement].name = `${lastPerson} ${array[lastElement].name.trim().split(` `)[1]}`
+}
+const quickQuestions = [
+    {
+        name: `compactAnswer`,
+        type: `input`,
+        message: `\nEnter employee in the following format (separated by a comma):\n- FULL NAME,\n- ID (MUST BE WHOLE NUMBER),\n- EMAIL,\n- ROLE (Manager, Engineer, or Intern),\n- TIDBIT (School if Intern, GitHub Username if Engineer, Office # if Manager)\n\nEXAMPLE: Big Bird, 7, bigboss@sesamestreet.com, engineer, hugeyellow7\n\n`,
+        validate: async input => {
+            if (!input) {
+                return `An input is required`
+            }
+            return true
+        },
+    },
+    {
+        name: `addMore`,
+        type: `confirm`,
+        message: `Add another employee?`,
+        default: false,
+    }
+]
+
+const askQuickUser = async () => {
+    try {
+        const { projectname } = await inquirer.prompt(questions)
+        let keepAsking = true
+
+        while (keepAsking) {
+            // count++
+            const { compactAnswer, addMore } = await inquirer.prompt(quickQuestions)
+            const eArray = compactAnswer.split(`,`).map(data => data.trim())
+            // console.log(eArray)
+            const role = eArray[3]
+            const name = eArray[0]
+            const id = eArray[1]
+            const email = eArray[2]
+            const wildcard = eArray[eArray.length - 1]
+
             console.log(separator)
             // make new employee obj depending on role of employee (using answers)
-            switch (role) {
-                case `Manager`:
-                    employeesArray.push(new Manager(name, id, email, officenum))
+            switch (role.toLowerCase()) {
+                case `manager`:
+                    employeesArrayQuick.push(new Manager(name, id, email, wildcard))
                     break
-                case `Engineer`:
-                    employeesArray.push(new Engineer(name, id, email, github))
+                case `engineer`:
+                    employeesArrayQuick.push(new Engineer(name, id, email, wildcard))
                     break
-                case `Intern`:
-                    employeesArray.push(new Intern(name, id, email, school))
+                case `intern`:
+                    employeesArrayQuick.push(new Intern(name, id, email, wildcard))
                     break
                 default:
-                    employeesArray.push(new Employee(name, id, email))
+                    employeesArrayQuick.push(new Employee(name, id, email))
                     break
             }
 
-            if (!askAgain) {
+            if (!addMore) {
                 keepAsking = false
-                let allMembers = employeesArray.map(data => data.name.split(` `)[0]).join(`, `)
-                let lastPerson = employeesArray[employeesArray.length - 1].name.trim().split(` `)[0]
-                // if there is more than one team member, then find the last person and adhere an ampersond adding emphasis and alliteration
-                if (employeesArray.length > 2) {
-                    allMembers = allMembers.replace(lastPerson, `& ${lastPerson}`)
-                } else if (employeesArray.length > 1) {
-                    allMembers = allMembers.replace(`, ${lastPerson}`, ` & ${lastPerson}`)
-                }
-                console.log(`\nThank you for using Team-Tree Generator.\n\nThe information for ${allMembers} will be used to fill a simple web page.\n\nCheck for an "output" folder.\n\\^_^/`)
+                renderMemberList(employeesArrayQuick)
             }
         }
-        employeesArray.unshift(projectname)
-        const renderedhtml = render(employeesArray)
+        employeesArrayQuick.unshift(projectname)
+        const renderedhtml = render(employeesArrayQuick)
 
         fs.writeFile(outputPath, renderedhtml, err => {
             if (err) throw err
         })
+
     }
     catch (err) {
         console.log(err)
     }
 }
-const teamMembers = (array) => {
-    array.forEach(i => i.name)
-}
 const askDefaultUser = () => {
-    questionsTeam[1 - 1].default = `Gustavo Valenzuela`
+
+    questionsTeam[1 - 1].default = `Fakey McFakeFace`
     questionsTeam[3 - 1].default = 6699
     questionsTeam[4 - 1].default = `sample@gmail.com`
     questionsTeam[5 - 1].default = `DA STREETZ`
@@ -185,12 +313,27 @@ const askDefaultUser = () => {
     questionsTeam[questionsTeam.length - 1].default = false
     askUser()
 }
+
+// allowing for optional commands
 let option = process.argv[2]
 if (option === `-d`) {
     askDefaultUser()
+} else if (option === `-q`) {
+    askQuickUser()
 } else {
     askUser()
 }
+// const askOnebyOne = async (projectname) => {
+//     let keepAsking = true
+//     try {
+
+//     } catch (err) {
+//         console.log(err)
+//     }
+
+// }
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
